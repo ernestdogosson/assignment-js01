518b9d5232230e86b9ebf9819d27d3a95a35a9c0
Ernest Dogo <dogoernest@yahoo.com>
2025-10-13 10:33:32 +0200

    create variables, perform arithmetic operations and print to console

--- script/index.js
+++ script/index.js
@@ -1,2 +1,27 @@
+// Exercise 1
+let greetings = 'Hello world!';
 
+console.log(greetings);
 
+// Exercise 2
+
+let name = 'Ernest';
+let age = 35;
+let favoriteColor = 'Any';
+
+console.log(
+	`My name is ${name} and my favorite color is ${favoriteColor} color  that works for me. I am ${age} years old.`
+);
+
+// Exercise 3
+let x = 3;
+let y = 9;
+
+let areaRectangle = x * y;
+let areaTriangle = y * 0.5 * 3;
+
+console.log(`x + y = ${x + y}`);
+console.log(`y - x = ${y - x}`);
+console.log(`y / x = ${y / x}`);
+console.log('The area of the rectangle is', areaRectangle);
+console.log(`The area of the triangle is ${y * 0.5 * 3}`);
