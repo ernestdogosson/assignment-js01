dd34bdf4dfb51b3f5dd697bd936c135b7895ea04
Ernest Dogo <dogoernest@yahoo.com>
2025-10-22 11:14:50 +0200

    add exercise 2

--- script/js-04.js
+++ script/js-04.js
@@ -1,17 +1,30 @@
 // Exercise 1
-function guessNumberGame() {
-	let compGuess = Math.floor(Math.random() * 10) + 1;
-	let userGuess;
+// function guessNumberGame() {
+// 	let compGuess = Math.floor(Math.random() * 10) + 1;
+// 	let userGuess;
 
-	do {
-		userGuess = parseFloat(prompt('Guess a number between 1 and 10'));
+// 	do {
+// 		userGuess = parseFloat(prompt('Guess a number between 1 and 10'));
 
-		if (compGuess != userGuess) {
-			alert(`You guessed ${userGuess}. Try again!`);
-		}
-	} while (compGuess != userGuess);
+// 		if (compGuess != userGuess) {
+// 			alert(`You guessed ${userGuess}. Try again!`);
+// 		}
+// 	} while (compGuess != userGuess);
 
-	console.log(
-		`Whoop! Whoop! The number is ${compGuess} and your guess is ${userGuess}. You win!`
-	);
+// 	console.log(
+// 		`Whoop! Whoop! The number is ${compGuess} and your guess is ${userGuess}. You win!`
+// 	);
+// }
+
+// Exercise 2
+function generateRandomArray(number) {
+	const arr = [];
+	number = parseInt(prompt('Enter a length of array.'));
+
+	for (let i = 1; i <= number; i++) {
+		arr.push(Math.floor(Math.random() * 100) + 1);
+	}
+	console.log(arr);
 }
+
+generateRandomArray();
