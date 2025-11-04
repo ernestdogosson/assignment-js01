6912c318defe6ead8d8c9e2931de6314717051e1
Ernest Dogo <dogoernest@outlook.com>
2025-11-04 09:03:53 +0100

    add getAccountName function and atm fuctions

--- script/js-05.js
+++ script/js-05.js
@@ -2,13 +2,54 @@ const account = {
   accountName: 'Jane Doe',
   balance: 10000,
   getBalace() {
-    return `${this.balance}`;
+    return this.balance;
   },
-  withdrawal(amount) {
+  deposit() {
+    let amount = parseFloat(prompt('Enter amount'));
+  },
+
+  withdrawal() {
+    let amount = parseFloat(prompt('Enter amount'));
+    let newBalance;
+
     if (amount > 0 || amount <= this.getBalace()) {
-      return console.log(this.getBalace() - amount);
+      newBalance = this.getBalace() - amount;
+
+      return newBalance;
+    }
+  },
+  getAccountName() {
+    return this.accountName;
+  },
+  accountError() {
+    if (this.getAccountName() !== 'Jane Doe') {
+      console.log('Invalid Account Name');
     }
   },
 };
 
-account.withdrawal(10000);
+// account.withdrawal(8000);
+
+function atm() {
+  const message = parseFloat(
+    prompt(
+      'Select a choice 1.) See balance 2.) Make a deposit 3.) Make withdral 4.) Get account name 5.) Exit'
+    )
+  );
+
+  switch (message) {
+    case 1:
+      console.log(account.getBalace());
+      break;
+    case 2:
+      console.log(account.deposit());
+      break;
+    case 3:
+      console.log(account.withdrawal());
+      break;
+    case 5:
+      console.log(account.getAccountName());
+  }
+}
+
+atm();
